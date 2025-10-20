647c3f1cf868c3f05bd28de3adb3ce80a3277fa7
Ahmad Faryab Kokab <68786248+afaryab@users.noreply.github.com>
2025-10-20 17:55:49 +0000

    Add dedoc/scramble package for API documentation generation
    - Updated composer.json to require dedoc/scramble version ^0.12.36.
    - Updated composer.lock to include dedoc/scramble and its dependencies.
    - Added configuration file for scramble with default settings for API documentation.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Scramble::configure()
+            ->routes(function (Route $route) {
+                return Str::startsWith($route->uri, 'api/');
+            });
     }
 }
